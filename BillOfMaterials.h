84b12e21ae894287867a4493e20e568d95d6b371
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2013-05-16 14:54:41 -0300

    minor tweak in BOM output formatting style

--- BillOfMaterials.h
+++ BillOfMaterials.h
@@ -21,7 +21,7 @@ compact=false;
 module BillOfMaterials(partname, quantity=1, category=false){
   for (q =[1:quantity]){
     if (category && compact){
-      echo(str("BOM: ", category));
+      echo(str("BOM: [", category, "]"));
     } else {
       if (category){
         echo(str("BOM: [", category, "] ", partname));
