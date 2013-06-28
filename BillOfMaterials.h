5f8327e4d74b5c01e0dbe93c29074ec336504d64
Rodrigo Rodrigues da Silva <rsilva@metamaquina.com.br>
2013-06-28 15:43:39 -0300

    Refactoring BOM module - add part ref and additional checks

--- BillOfMaterials.h
+++ BillOfMaterials.h
@@ -1,7 +1,8 @@
 // (c) 2013 Metamáquina <http://www.metamaquina.com.br/>
 //
-// Author:
+// Authors:
 // * Felipe C. da S. Sanches <fsanches@metamaquina.com.br>
+// * Rodrigo Rodrigues da Silva <rsilva@metamaquina.com.br>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,19 +17,36 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-compact=false;
-
-module BillOfMaterials(partname, quantity=1, category=false){
-  for (q =[1:quantity]){
-    if (category && compact){
-      echo(str("BOM: [", category, "]"));
-    } else {
-      if (category){
-        echo(str("BOM: [", category, "] ", partname));
-      } else {
-        echo(str("BOM: ", partname));
-      }
+module BillOfMaterials(partname="", quantity=1, category=false, ref=false){
+  if (len (str(partname)) || len(ref)){
+    for (q = [1:quantity]){
+      if (ref && category)
+        echo (str("BOM: ", "[", ref, "] ",  "{", category,"} ", partname));
+      else if (ref)
+        echo (str("BOM: ", "[", ref, "] ", partname));
+      else if (category)
+        echo(str("BOM: ", "{", category,"} ", partname));
+      else
+        echo (str("BOM: ", partname));
     }
   }
 }
 
+//TEST
+
+module test_BillOfMaterials(){
+  BillOfMaterials("TEST Simple part");
+  BillOfMaterials("TEST Part with quantity 3", 3);
+  BillOfMaterials("TEST Part with ref and category", 1, "The Cool Parts' Category", "COOLPART");
+  BillOfMaterials("TEST Part with category only", 2, "The Cool Parts' Category");
+  BillOfMaterials("TEST Another part no category", ref = "REFFOO", quantity = 2);
+  BillOfMaterials(ref="H_M4x70", quantity=12);
+
+  //no output
+  BillOfMaterials(quantity=3);
+  BillOfMaterials("");
+  BillOfMaterials(category="foo");
+  BillOfMaterials(partname="", ref="", quantity=20);
+}
+
+//test_BillOfMaterials();
